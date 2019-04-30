2aad5315f6a9d5e0a3cd280eb6f6c85fbdeac67c
Taku Nakagawa <taku_nakagawa@change-jp.com>
2019-04-30 14:16:09 +0900

    app.componentの微調整

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Taku Nakagawa';
   links = ['Profile', 'Works', 'Skills', 'Blogs'];
-  activeLinks = this.links[0];
+  activeLink = this.links[0];
 }
