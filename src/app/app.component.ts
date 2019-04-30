459a4f995b3c84b84e40caf7954da994053147f5
Taku Nakagawa <taku_nakagawa@change-jp.com>
2019-05-01 01:04:43 +0900

    Interfaceの実装

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Links } from './interface';
 
 @Component({
   selector: 'app-root',
@@ -6,23 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  selectedTab = '';
+  selectedTab;
 
   title = 'Taku Nakagawa';
-  links = [
-    { name: 'Profile', urlPath: 'profile' },
-    { name: 'Works', urlPath: 'works' },
-    { name: 'Skills', urlPath: 'skills' },
-    { name: 'Blogs', urlPath: 'blogs' },
+  links: Links[] = [
+    { name: 'Profile', urlPath: 'profile', activedTab: false },
+    { name: 'Works', urlPath: 'works', activedTab: false },
+    { name: 'Skills', urlPath: 'skills', activedTab: false },
+    { name: 'Blogs', urlPath: 'blogs', activedTab: false },
   ];
 
   onSelect(link) {
-    this.selectedTab = link;
-    console.log(this.selectedTab);
+    for (const value of this.links) {
+      value.activedTab = false;
+      if (value.name === link.name) {
+        value.activedTab = true;
+      }
+      console.log(this.links);
+    }
   }
 
   ngOnInit() {
-    console.log('ngOnInit');
+    console.log(this.links);
   }
 
 }
---
+++ src/app/interface.ts
@@ -0,0 +1,5 @@
+export class Links {
+    name: string;
+    urlPath: string;
+    activedTab: boolean;
+}
