c76e71673539a88c7a59e40e07bc929104add763
Taku Nakagawa <taku_nakagawa@change-jp.com>
2019-04-30 14:52:38 +0900

    Route Moduleに追加

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { ProfilesComponent } from './profiles/profiles.component';
+import { WorksComponent } from './works/works.component';
+import { SkillsComponent } from './skills/skills.component';
+import { BlogsComponent } from './blogs/blogs.component';
 
 const routes: Routes = [
-  {path: '', component : ProfilesComponent}
+  {path: '', component : ProfilesComponent},
+  { path: 'works', component: WorksComponent },
+  { path: 'skills', component: SkillsComponent },
+  { path: 'blogs', component: BlogsComponent },
 ];
 
 @NgModule({
