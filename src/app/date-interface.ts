a166c7833aa1ca205205ee811f92270ce5dc472f
Taku Nakagawa <taku_nakagawa@change-jp.com>
2019-05-02 21:58:37 +0900

    Blog欄を削除

--- src/app/date-interface.ts
+++ src/app/date-interface.ts
@@ -4,7 +4,7 @@ export let LINKS: Links[] = [
     { name: 'Profile', urlPath: 'profile', activedTab: true },
     { name: 'Works', urlPath: 'works', activedTab: false },
     { name: 'Experience', urlPath: 'experience', activedTab: false },
-    { name: 'Blogs', urlPath: 'blogs', activedTab: false },
+    // { name: 'Blogs', urlPath: 'blogs', activedTab: false },
 ];
 
 export const EXPERIENCE: ExperienceElemnt[] = [
